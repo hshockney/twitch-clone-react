7eff4b1e601d3ec133ceadcf1ba677da0e4cdd34
hshockney <shockney@atlanticbb.net>
2019-07-02 14:57:41 -0400

    Refactored googleauth to use actions

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
     state = { isSignedIn: null };
@@ -14,8 +16,12 @@ class GoogleAuth extends React.Component {
             });
         });
     }
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
     }
     onSignInClick = () => {
         this.auth.signIn() ;   
@@ -38,4 +44,4 @@ class GoogleAuth extends React.Component {
         );
     }
 }
-export default GoogleAuth;
+export default connect(null, {signIn, signOut})(GoogleAuth);
