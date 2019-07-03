862dfcdb66d0cb78a4498751b189ac544e1d5a00
hshockney <shockney@atlanticbb.net>
2019-07-03 09:51:20 -0400

    Added validation mathod to make sure user entered required info

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -10,14 +10,29 @@ class StreamCreate extends React.Component {
             </div>
         )
     }
+    onSubmit(formValues) {
+        
+    }
     render() {
         return(
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Enter Title:" />
                 <Field name="description" component={this.renderInput} label="Enter Description:" />
+                    <button className="ui button primary">Submit</button>
             </form>
         );
     }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title){
+        errors.title = 'You must enter a title';
+    } 
+    if (!formValues.description){
+        errors.description = 'You must enter a description';
+    }     
+    return errors;
 }
 export default reduxForm({
     form: 'streamCreate'
