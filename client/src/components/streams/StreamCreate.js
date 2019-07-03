a22e04f66babb51bc21f1b2b3a3a0236044686d6
hshockney <shockney@atlanticbb.net>
2019-07-03 15:56:19 -0400

    Added new action and hooke dup createstream to server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
     renderError({ error, touched }) {
@@ -21,8 +23,8 @@ class StreamCreate extends React.Component {
             </div>
         )
     }
-    onSubmit(formValues) {
-        
+    onSubmit = formValues => {
+        this.props.createStream(formValues);  
     }
     render() {
         return(
@@ -45,7 +47,10 @@ const validate = (formValues) => {
     }     
     return errors;
 }
-export default reduxForm({
+
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
