e9ee9015f393c9c7c0321296566b4803d00aeec0
hshockney <shockney@atlanticbb.net>
2019-07-03 14:21:10 -0400

    Showing error messages to screen based onfocusout

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label }){
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className=" ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput = ({ input, label, meta }) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} />
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
         )
     }
@@ -15,7 +26,7 @@ class StreamCreate extends React.Component {
     }
     render() {
         return(
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Enter Title:" />
                 <Field name="description" component={this.renderInput} label="Enter Description:" />
                     <button className="ui button primary">Submit</button>
@@ -35,5 +46,6 @@ const validate = (formValues) => {
     return errors;
 }
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate);
