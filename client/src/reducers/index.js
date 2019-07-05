5b02aa52716633c7cf1856eef85f9a6878f74093
hshockney <shockney@atlanticbb.net>
2019-07-05 10:19:07 -0400

    Displaying streams to screen, added reducer for streamlist

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
+import streamReducer from './streamsReducer'
 import authReducer from './authReducer';
+
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
