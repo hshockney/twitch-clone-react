5c721f1b28736adbd0b5f834996c28f74edbaeb4
hshockney <shockney@atlanticbb.net>
2019-07-08 10:19:33 -0400

    Added react fragment to avoid unneeded div messing up styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import history from '../history';
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                Modal
+        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+            <div onClick={(event) => event.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">
+                    <h2>{props.title}</h2>
+                </div>
+                <div className="content">
+                    <h3>{props.content}</h3>
+                </div>
+                <div className="actions">
+                    {props.actions}
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')   
